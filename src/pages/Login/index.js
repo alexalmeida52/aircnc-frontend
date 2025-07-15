f028f7bc4af800941003426a44111192c20525fd
alexalmeida52 <jhosealex@gmail.com>
2025-07-14 22:35:24 -0300

    feat: setup Docker Compose with MongoDB and Node.js app

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react';
-import api from '../../services/api'
+import React, { useState }  from 'react'
+import api from '../../services/api.js'
 
 export default function Login({ history }) {
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState('')
 
     async function handleSubmit(event) {
-        event.preventDefault();
-        const response = await api.post('/sessions', { email });
-        const { _id } = response.data;
-        localStorage.setItem('user_id', _id);
-        console.log(_id);
-        history.push('/dashboard');
+      event.preventDefault()
+      
+      const response = await api.post('/sessions', {
+        email: email
+      })
+  
+      const { _id } = response.data
+  
+      localStorage.setItem('user', _id)
+
+      history.push('/dashboard')
     }
+    
+
     return (
         <>
             <p>
-                Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
             </p>
 
             <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email *</label>
-                <input
-                    id="email"
-                    type="email"
-                    placeholder="Seu melhor email"
-                    value={email}
-                    onChange={event => setEmail(event.target.value)} />
+            <label htmlFor="email">E-MAIL *</label>
+            <input
+                id="email"
+                type="email"
+                placeholder="Seu melhor e-mail"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+            />
 
-                <button className="btn" type="submit">Entrar</button>
+            <button className="btn" type="submit">Entrar</button>
             </form>
         </>
     )
